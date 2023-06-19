eb597dd7d454963c23b20a32d357de8b52516f6d
Brian Flynn <bflynn@battech.com>
2023-06-19 16:18:19 -0600

    modularizing dependencies

--- 01_aks_dependencies.tf
+++ 01_aks_dependencies.tf
@@ -12,16 +12,18 @@ output "aks_cluster_location" {
 # Create Azure Log Analytics Workspace
 resource "azurerm_log_analytics_workspace" "aks_log_analytics" {
   location            = var.aks_config.location
-  resource_group_name = azurerm_resource_group.aks_cluster_rg.name # #var.aks_cluster_rg
   sku                 = var.aks_config.log_analytics_workspace_sku
   name                = "${var.aks_config.name}-analytics" #"aks-cluster-log-analytics"
+  resource_group_name = azurerm_resource_group.aks_cluster_rg.name # #var.aks_cluster_rg
 }
 output "aks_log_analytics_workspace_id" {
   value = azurerm_log_analytics_workspace.aks_log_analytics.id
 }
-# Define user assigned identities
-resource "azurerm_user_assigned_identity" "aks_cluster_identity" {
+
+
+module "user_assigned_identity" {
+  source = "./modules/user_assigned_identity"
   location            = var.aks_config.location
-  resource_group_name = azurerm_resource_group.aks_cluster_rg.name
   name                = "${var.aks_config.name}-identity" # "aks-cluster-identity"
-}
+  resource_group_name = azurerm_resource_group.aks_cluster_rg.name
+  }
