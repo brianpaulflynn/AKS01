53ff77b9a72bcf3caf8a792baa5452b32efb78c9
Brian Flynn <bflynn@battech.com>
2023-06-19 17:45:10 -0600

    modularizing node pool

--- 04_aks_nodes.tf
+++ 04_aks_nodes.tf
@@ -1,20 +1,21 @@
 # # Suggestion: Define Availability Sets for node_pools to improve resiliency.
+module "node_pool_1" {
+    source = "./modules/aks_node_pool"
+    kubernetes_cluster_id = module.aks_cluster.aks_cluster_id # azurerm_kubernetes_cluster.aks_cluster.id
+    vnet_subnet_id        = module.aks_subnets.subnet_ids["aks_node_subnet_1"] #azurerm_subnet.aks_subnets["aks_node_subnet_1"].id
+    pod_subnet_id         = module.aks_subnets.subnet_ids["aks_pod_subnet_1"] #azurerm_subnet.aks_subnets["aks_pod_subnet_1"].id
+    enable_auto_scaling   = true
+    # Configurable by module
+    name                  = "pool1"
+    vm_size               = "Standard_B2s"
+    os_disk_size_gb       = 30
+    zones                 = [ 1 , 2 , 3 ]
+    min_count             = 1
+    max_count             = 3
+    max_pods              = 32
+    Environment         = "Pool1Tag"
+}
 # resource "azurerm_kubernetes_cluster_node_pool" "node_pool_1" {
-#   kubernetes_cluster_id = azurerm_kubernetes_cluster.aks_cluster.id
-#   vnet_subnet_id        = azurerm_subnet.aks_subnets["aks_node_subnet_1"].id
-#   pod_subnet_id         = azurerm_subnet.aks_subnets["aks_pod_subnet_1"].id
-#   enable_auto_scaling   = true
-#   # Configurable by module
-#   name                  = "pool1"
-#   vm_size               = "Standard_B2s"
-#   os_disk_size_gb       = 30
-#   zones                 = [ 1 , 2 , 3 ]
-#   min_count             = 1
-#   max_count             = 3
-#   max_pods              = 32
-#   tags = {
-#     Environment         = "Pool1Tag"
-#   }
 # }
 
 # resource "azurerm_kubernetes_cluster_node_pool" "node_pool_2" {
