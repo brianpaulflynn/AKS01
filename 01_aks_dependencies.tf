065ee38758b031a8d1099675414aa45533f9014b
Brian Flynn <bflynn@battech.com>
2023-06-19 16:42:49 -0600

    modularizing dependencies

--- 01_aks_dependencies.tf
+++ 01_aks_dependencies.tf
@@ -9,19 +9,26 @@ output "aks_cluster_rg_name" {
 output "aks_cluster_location" {
   value = azurerm_resource_group.aks_cluster_rg.location
 }
-# Create Azure Log Analytics Workspace
-resource "azurerm_log_analytics_workspace" "aks_log_analytics" {
+
+# # Create Azure Log Analytics Workspace
+module "aks_log_analytics" {
+  source = "./modules/log_analytics_workspace"
   location            = var.aks_config.location
   sku                 = var.aks_config.log_analytics_workspace_sku
   name                = "${var.aks_config.name}-analytics" #"aks-cluster-log-analytics"
   resource_group_name = azurerm_resource_group.aks_cluster_rg.name # #var.aks_cluster_rg
 }
-output "aks_log_analytics_workspace_id" {
-  value = azurerm_log_analytics_workspace.aks_log_analytics.id
-}
+# resource "azurerm_log_analytics_workspace" "aks_log_analytics" {
+#   location            = var.aks_config.location
+#   sku                 = var.aks_config.log_analytics_workspace_sku
+#   name                = "${var.aks_config.name}-analytics" #"aks-cluster-log-analytics"
+#   resource_group_name = azurerm_resource_group.aks_cluster_rg.name # #var.aks_cluster_rg
+# }
+# output "aks_log_analytics_workspace_id" {
+#   value = azurerm_log_analytics_workspace.aks_log_analytics.id
+# }
 
-
-module "user_assigned_identity" {
+module "aks_cluster_identity" {
   source = "./modules/user_assigned_identity"
   location            = var.aks_config.location
   name                = "${var.aks_config.name}-identity" # "aks-cluster-identity"
--- modules/user_assigned_identity/main.tf
+++ modules/user_assigned_identity/main.tf
@@ -1,5 +1,8 @@
-resource "azurerm_user_assigned_identity" "aks_cluster_identity" {
+resource "azurerm_user_assigned_identity" "this" {
   location            = var.location # var.aks_config.location
   name                = var.name # "${var.aks_config.name}-identity" # "aks-cluster-identity"
   resource_group_name = var.resource_group_name # azurerm_resource_group.aks_cluster_rg.name
+}
+output "identity_id" {
+  value = azurerm_user_assigned_identity.this.id
 }
