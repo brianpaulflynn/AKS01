36b207f0e41c0aaf7c90c99307a62f3b27531ac5
Brian Flynn <bflynn@battech.com>
2023-06-20 00:06:11 -0600

    code consolidations

--- 10_network.tf
+++ 10_network.tf
@@ -1,3 +1,16 @@
+# Define the virtual network and subnets for AKS
+module "aks_vnet" {
+  source              = "./modules/vnet"
+  resource_group_name = module.aks_cluster_rg.rg_name
+  location            = module.aks_cluster_rg.rg_location
+  name                = var.aks_config.vnet_name
+  address_space       = [var.aks_config.vnet_cidr]
+}
+module "aks_subnets" {
+  source               = "./modules/subnet"
+  resource_group_name  = module.aks_cluster_rg.rg_name
+  virtual_network_name = module.aks_vnet.vnet_name
+}
 # Define the AKS network security group (NSG)
 module "aks_nsg" {
   source              = "./modules/nsg"
