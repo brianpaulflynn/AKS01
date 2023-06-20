6ea7c0f8d97b18b57051d1dd716f101a1b375c8c
Brian Flynn <bflynn@battech.com>
2023-06-20 00:25:58 -0600

    code cleanups

--- modules/aks_node_pool/maint.tf
+++ modules/aks_node_pool/maint.tf
@@ -1,17 +1,17 @@
 resource "azurerm_kubernetes_cluster_node_pool" "node_pool_1" {
-  kubernetes_cluster_id = var.kubernetes_cluster_id #azurerm_kubernetes_cluster.aks_cluster.id
-  vnet_subnet_id        = var.vnet_subnet_id #azurerm_subnet.aks_subnets["aks_node_subnet_1"].id
-  pod_subnet_id         = var.pod_subnet_id #azurerm_subnet.aks_subnets["aks_pod_subnet_1"].id
-  enable_auto_scaling   = var.enable_auto_scaling #true
+  kubernetes_cluster_id = var.kubernetes_cluster_id
+  vnet_subnet_id        = var.vnet_subnet_id
+  pod_subnet_id         = var.pod_subnet_id
+  enable_auto_scaling   = var.enable_auto_scaling
   # Configurable by module
-  name                  = var.name #"pool1"
-  vm_size               = var.vm_size #"Standard_B2s"
-  os_disk_size_gb       = var.os_disk_size_gb #30
-  zones                 = var.zones #[ 1 , 2 , 3 ]
-  min_count             = var.min_count #1
-  max_count             = var.max_count #3
-  max_pods              = var.max_pods #32
+  name                  = var.name 
+  vm_size               = var.vm_size
+  os_disk_size_gb       = var.os_disk_size_gb 
+  zones                 = var.zones 
+  min_count             = var.min_count 
+  max_count             = var.max_count 
+  max_pods              = var.max_pods 
   tags = {
-    Environment         = var.Environment # "Pool1Tag"
+    Environment         = var.Environment
   }
 }
--- modules/nsga/main.tf
+++ modules/nsga/main.tf
@@ -1,9 +1,7 @@
 resource "azurerm_subnet_network_security_group_association" "subnets_nsg_association" {
-  #for_each                      = var.subnets_map # var.aks_config.subnets_map
-  subnet_id                     = var.subnet_id # module.aks_subnets.subnet_ids[each.key]
-  network_security_group_id     = var.network_security_group_id # module.aks_nsg.network_security_group_id
+  subnet_id                     = var.subnet_id 
+  network_security_group_id     = var.network_security_group_id 
 }
 output "network_security_group_id" {
- value = azurerm_subnet_network_security_group_association.subnets_nsg_association.id
-          #{ for subnet_name, subnet in azurerm_subnet_network_security_group_association.subnets_nsg_association : subnet_name => subnet.id }
+  value = azurerm_subnet_network_security_group_association.subnets_nsg_association.id
 }
