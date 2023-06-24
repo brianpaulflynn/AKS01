56299b031bb1a76fe1d460ea5a7693989f9eb0df
Brian Flynn <bflynn@battech.com>
2023-06-24 07:43:02 -0600

    tweaks

--- terraform/20_aks.tf
+++ terraform/20_aks.tf
@@ -11,34 +11,18 @@ module "aks_cluster" {
 # Suggestion: Define Availability Sets for node_pools to improve resiliency.
 module "aks_node_pool_1" {
   # ADD FOR EACH HERE THAT USES A MAP IN THE aks_config VARIABLE
-  source                = "../modules/aks_node_pool"
-  kubernetes_cluster_id = module.aks_cluster.aks_cluster_id
-  vnet_subnet_id        = module.aks_subnets.subnet_ids["aks_node_subnet_1"]
-  pod_subnet_id         = module.aks_subnets.subnet_ids["aks_pod_subnet_1"]
+  source                = "../modules/aks_node_pool" # we may want many different node pools defined in modules that all inherit this.
   max_pods              = 32              # CONSIDER: make this not exposed. Make it determined by network math. 2^(node_mask/pod_mask)
   vm_size               = "Standard_B2s"  # CONSIDER: holding back in modules defined to expose configured skus to developers.
   os_disk_size_gb       = 30              # CONSIDER: holding back in modules defined to expose configured skus to developers.
   zones                 = [1, 2, 3]       # CONSIDER: holding back in modules defined to expose configured skus to developers.
   enable_auto_scaling   = true            # CONSIDER: holding back in modules defined to expose configured skus to developers.
 
-  name                  = "pool1"         # CONSIDER: making available to developers.
-  Environment           = "Pool1Tag"      # CONSIDER: making available to developers.
-  min_count             = 1               # CONSIDER: making available to developers.
-  max_count             = 3               # CONSIDER: making available to developers.
+  kubernetes_cluster_id = module.aks_cluster.aks_cluster_id
+  vnet_subnet_id        = module.aks_subnets.subnet_ids["aks_node_subnet_1"]    # var.aks_config.???
+  pod_subnet_id         = module.aks_subnets.subnet_ids["aks_pod_subnet_1"]     # var.aks_config.???
+  name                  = "pool1"         # CONSIDER: making available to developers via var.aks_config.
+  Environment           = "Pool1Tag"      # CONSIDER: making available to developers via var.aks_config.
+  min_count             = 1               # CONSIDER: making available to developers via var.aks_config.
+  max_count             = 3               # CONSIDER: making available to developers via var.aks_config.
 }
-# module "aks_node_pool_2" {
-#   source                = "../modules/aks_node_pool"
-#   kubernetes_cluster_id = module.aks_cluster.aks_cluster_id
-#   vnet_subnet_id        = module.aks_subnets.subnet_ids["aks_node_subnet_2"]
-#   pod_subnet_id         = module.aks_subnets.subnet_ids["aks_pod_subnet_2"]
-#   max_pods              = 32              # CONSIDER: make this not exposed. Make it determined by network math. 2^(node_mask/pod_mask)
-#   vm_size               = "Standard_B2s"  # CONSIDER: holding back in modules defined to expose configured skus to developers.
-#   os_disk_size_gb       = 30              # CONSIDER: holding back in modules defined to expose configured skus to developers.
-#   zones                 = [1, 2, 3]       # CONSIDER: holding back in modules defined to expose configured skus to developers.
-#   enable_auto_scaling   = true            # CONSIDER: holding back in modules defined to expose configured skus to developers.
-
-#   name                  = "pool2"         # CONSIDER: making available to developers.
-#   Environment           = "Pool2Tag"      # CONSIDER: making available to developers.
-#   min_count             = 1               # CONSIDER: making available to developers.
-#   max_count             = 3               # CONSIDER: making available to developers.
-# }
