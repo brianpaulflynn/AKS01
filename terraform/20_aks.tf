233656a0a3eb24470f7eee16e63b5903d56df2b9
Brian Flynn <bflynn@battech.com>
2023-06-24 20:08:04 -0600

    comments

--- terraform/20_aks.tf
+++ terraform/20_aks.tf
@@ -16,16 +16,19 @@ module "aks_node_pool_1" {
   kubernetes_cluster_id = module.aks_cluster.aks_cluster_id
   vnet_subnet_id        = module.aks_subnets.aks_node_subnet_ids["aks_user_pool_1"]
   pod_subnet_id         = module.aks_subnets.aks_pod_subnet_ids["aks_user_pool_1"]
-  max_pods              = 32             # CONSIDER: make this not exposed. Make it determined by network math. 2^(node_mask/pod_mask)
-  vm_size               = "Standard_B2s" # CONSIDER: holding back in modules defined to expose configured skus to developers.
-  os_disk_size_gb       = 30             # CONSIDER: holding back in modules defined to expose configured skus to developers.
-  zones                 = [1, 2, 3]      # CONSIDER: holding back in modules defined to expose configured skus to developers.
-  enable_auto_scaling   = true           # CONSIDER: holding back in modules defined to expose configured skus to developers.
-
-  name        = "pool1"    # CONSIDER: making available to developers via var.aks_config.
-  Environment = "Pool1Tag" # CONSIDER: making available to developers via var.aks_config.
-  min_count   = 1          # CONSIDER: making available to developers via var.aks_config.
-  max_count   = 3          # CONSIDER: making available to developers via var.aks_config.
+  max_pods              = 32 # Needs to be determined by network math. 2^(node_mask/pod_mask)
+  # ex: /27 & /22 are /5 apart.  That makes for 32:1 pods:nodes.
+  #     /29 is the smallest Azure subnet. Provides 3 usable IPs.
+  # CONSIDER: holding back in modules defined to expose configured skus to developers.
+  zones               = [1, 2, 3] # Use all zones for max resiliency.
+  vm_size             = "Standard_B2s"
+  os_disk_size_gb     = 30
+  enable_auto_scaling = true
+  # CONSIDER: making available to developers via var.aks_config.
+  name        = "pool1"
+  Environment = "Pool1Tag"
+  min_count   = 1
+  max_count   = 3
 }
 module "aks_node_pool_2" {
   # ADD FOR EACH HERE THAT USES A MAP IN THE aks_config VARIABLE
@@ -33,14 +36,17 @@ module "aks_node_pool_2" {
   kubernetes_cluster_id = module.aks_cluster.aks_cluster_id
   vnet_subnet_id        = module.aks_subnets.aks_node_subnet_ids["aks_user_pool_2"]
   pod_subnet_id         = module.aks_subnets.aks_pod_subnet_ids["aks_user_pool_2"]
-  max_pods              = 32             # CONSIDER: make this not exposed. Make it determined by network math. 2^(node_mask/pod_mask)
-  vm_size               = "Standard_B2s" # CONSIDER: holding back in modules defined to expose configured skus to developers.
-  os_disk_size_gb       = 30             # CONSIDER: holding back in modules defined to expose configured skus to developers.
-  zones                 = [1, 2, 3]      # CONSIDER: holding back in modules defined to expose configured skus to developers.
-  enable_auto_scaling   = true           # CONSIDER: holding back in modules defined to expose configured skus to developers.
-
-  name        = "pool2"    # CONSIDER: making available to developers via var.aks_config.
-  Environment = "Pool2Tag" # CONSIDER: making available to developers via var.aks_config.
-  min_count   = 1          # CONSIDER: making available to developers via var.aks_config.
-  max_count   = 3          # CONSIDER: making available to developers via var.aks_config.
+  max_pods              = 32 # Needs to be determined by network math. 2^(node_mask/pod_mask)
+  # ex: /27 & /22 are /5 apart.  That makes for 32:1 pods:nodes.
+  #     /29 is the smallest Azure subnet. Provides 3 usable IPs.
+  # CONSIDER: holding back in modules defined to expose configured skus to developers.
+  zones               = [1, 2, 3] # Use all zones for max resiliency.
+  vm_size             = "Standard_B2s"
+  os_disk_size_gb     = 30
+  enable_auto_scaling = true
+  # CONSIDER: making available to developers via var.aks_config.
+  name        = "pool2"
+  Environment = "Pool2Tag"
+  min_count   = 1
+  max_count   = 3
 }
