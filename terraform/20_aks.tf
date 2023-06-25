5aa87da55497146abecb354b03987cd9db977aac
Brian Flynn <bflynn@battech.com>
2023-06-24 23:33:45 -0600

    for_each node pools

--- terraform/20_aks.tf
+++ terraform/20_aks.tf
@@ -9,18 +9,18 @@ module "aks_cluster" {
   aks_managed_identity_ids       = [module.aks_cluster_identity.identity_id]
 }
 module "aks_node_pool_1" {
-  # ADD FOR EACH HERE THAT USES A MAP IN THE aks_config VARIABLE
   source                = "../modules/aks_node_pool"
+  for_each              = var.aks_config.node_pool_map
   kubernetes_cluster_id = module.aks_cluster.aks_cluster_id
-  vnet_subnet_id        = module.aks_subnets.aks_node_subnet_ids["aks_user_pool_1"]
-  pod_subnet_id         = module.aks_subnets.aks_pod_subnet_ids["aks_user_pool_1"]
-  vm_size               = var.aks_config.node_pool_map["aks_user_pool_1"].vm_size
-  zones                 = var.aks_config.node_pool_map["aks_user_pool_1"].zones
-  enable_auto_scaling   = var.aks_config.node_pool_map["aks_user_pool_1"].enable_auto_scaling
-  name                  = var.aks_config.node_pool_map["aks_user_pool_1"].name
-  Environment           = var.aks_config.node_pool_map["aks_user_pool_1"].Environment
-  os_disk_size_gb       = var.aks_config.node_pool_map["aks_user_pool_1"].os_disk_size_gb
-  min_count             = var.aks_config.node_pool_map["aks_user_pool_1"].min_count
-  max_count             = var.aks_config.node_pool_map["aks_user_pool_1"].max_count
-  max_pods              = var.aks_config.node_pool_map["aks_user_pool_1"].max_pods
+  vnet_subnet_id        = module.aks_subnets.aks_node_subnet_ids[each.key]
+  pod_subnet_id         = module.aks_subnets.aks_pod_subnet_ids[each.key]
+  vm_size               = var.aks_config.node_pool_map[each.key].vm_size
+  zones                 = var.aks_config.node_pool_map[each.key].zones
+  enable_auto_scaling   = var.aks_config.node_pool_map[each.key].enable_auto_scaling
+  name                  = var.aks_config.node_pool_map[each.key].name
+  Environment           = var.aks_config.node_pool_map[each.key].Environment
+  os_disk_size_gb       = var.aks_config.node_pool_map[each.key].os_disk_size_gb
+  min_count             = var.aks_config.node_pool_map[each.key].min_count
+  max_count             = var.aks_config.node_pool_map[each.key].max_count
+  max_pods              = var.aks_config.node_pool_map[each.key].max_pods
 }
