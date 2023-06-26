7dab92f1a23564ed8d28a9541f5aba7c1f719ff4
Brian Flynn <bflynn@battech.com>
2023-06-26 17:03:35 -0600

    tidy. fixes. comments.

--- terraform/20_main.tf
+++ terraform/20_main.tf
@@ -1,5 +1,5 @@
 # Define the resource group
-module "aks_cluster_rg" {
+module "aks_rg" {
   source   = "../modules/rg"
   location = var.aks_config.location
   name     = var.aks_config.rg
@@ -7,33 +7,33 @@ module "aks_cluster_rg" {
 # Define the virtual network and subnets for AKS
 module "aks_vnet" {
   source              = "../modules/vnet"
-  resource_group_name = module.aks_cluster_rg.rg_name
-  location            = module.aks_cluster_rg.rg_location
+  resource_group_name = module.aks_rg.rg_name
+  location            = module.aks_rg.rg_location
   name                = var.aks_config.vnet_name
   address_space       = [var.aks_config.vnet_cidr]
 }
 module "aks_subnets" {
-  source               = "../modules/subnet"
-  resource_group_name  = module.aks_cluster_rg.rg_name
+  source               = "../modules/aks_subnet"
+  resource_group_name  = module.aks_rg.rg_name
   virtual_network_name = module.aks_vnet.vnet_name
   node_pool_map        = var.aks_config.node_pool_map
 }
 # Define the AKS network security group (NSG)
 module "aks_nsg" {
   source              = "../modules/nsg"
-  resource_group_name = module.aks_cluster_rg.rg_name
-  location            = module.aks_cluster_rg.rg_location
+  resource_group_name = module.aks_rg.rg_name
+  location            = module.aks_rg.rg_location
   name                = "${var.aks_config.name}-nsg"
 }
 # Define the NSG associations for each node subnet
-module "subnets_nsg_associations_nodes" {
+module "aks_subnets_nsg_associations_nodes" {
   source                    = "../modules/nsga"
   for_each                  = module.aks_subnets.aks_node_subnet_ids
   subnet_id                 = module.aks_subnets.aks_node_subnet_ids[each.key]
   network_security_group_id = module.aks_nsg.network_security_group_id
 }
 # Define the NSG associations for each pod subnet
-module "subnets_nsg_associations_pods" {
+module "aks_subnets_nsg_associations_pods" {
   source                    = "../modules/nsga"
   for_each                  = module.aks_subnets.aks_pod_subnet_ids
   subnet_id                 = module.aks_subnets.aks_pod_subnet_ids[each.key]
@@ -42,21 +42,21 @@ module "subnets_nsg_associations_pods" {
 # Define Network Security Group Rules for the AKS vnet
 module "aks_nsrs" {
   source                      = "../modules/aks_nsrs"
-  resource_group_name         = module.aks_cluster_rg.rg_name
+  resource_group_name         = module.aks_rg.rg_name
   network_security_group_name = module.aks_nsg.network_security_group_name
   node_pool_map               = var.aks_config.node_pool_map
 }
 # Create Azure Log Analytics Workspace
 module "aks_log_analytics" {
   source              = "../modules/log_analytics_workspace"
-  resource_group_name = module.aks_cluster_rg.rg_name
+  resource_group_name = module.aks_rg.rg_name
   location            = var.aks_config.location
   sku                 = var.aks_config.log_analytics_workspace_sku
   name                = "${var.aks_config.name}-analytics"
 }
 # Create aks cluster identity
 module "aks_cluster_identity" {
-  resource_group_name = module.aks_cluster_rg.rg_name
+  resource_group_name = module.aks_rg.rg_name
   source              = "../modules/user_assigned_identity"
   location            = var.aks_config.location
   name                = "${var.aks_config.name}-identity"
