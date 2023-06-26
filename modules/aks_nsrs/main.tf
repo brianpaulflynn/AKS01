43ca11b6aaf813a1f7914afd0c53140df0c0886b
Brian Flynn <bflynn@battech.com>
2023-06-26 15:36:00 -0600

    tidy. fixes. comments.

--- modules/aks_nsrs/main.tf
+++ modules/aks_nsrs/main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_network_security_rule" "allow_pod_subnet_outbound" {
   name                        = "allow-pod-subnet-outbound"
-  resource_group_name         = var.resource_group_name         # module.aks_cluster_rg.rg_name
-  network_security_group_name = var.network_security_group_name # module.aks_nsg.network_security_group_name
+  resource_group_name         = var.resource_group_name
+  network_security_group_name = var.network_security_group_name
   priority                    = 100
   direction                   = "Outbound"
   access                      = "Allow"
@@ -16,8 +16,8 @@ resource "azurerm_network_security_rule" "allow_pod_subnet_outbound" {
 }
 resource "azurerm_network_security_rule" "allow_pod_to_pod" {
   name                        = "allow-pod-to-pod-inbound"
-  resource_group_name         = var.resource_group_name         # module.aks_cluster_rg.rg_name
-  network_security_group_name = var.network_security_group_name # module.aks_nsg.network_security_group_name
+  resource_group_name         = var.resource_group_name
+  network_security_group_name = var.network_security_group_name
   priority                    = 100
   direction                   = "Inbound"
   access                      = "Allow"
@@ -35,8 +35,8 @@ resource "azurerm_network_security_rule" "allow_pod_to_pod" {
 }
 resource "azurerm_network_security_rule" "deny_node_to_pod_subnet" {
   name                        = "deny-node-to-pod-subnet"
-  resource_group_name         = var.resource_group_name         # module.aks_cluster_rg.rg_name
-  network_security_group_name = var.network_security_group_name # module.aks_nsg.network_security_group_name
+  resource_group_name         = var.resource_group_name
+  network_security_group_name = var.network_security_group_name
   priority                    = 101
   direction                   = "Inbound"
   access                      = "Deny"
@@ -54,8 +54,8 @@ resource "azurerm_network_security_rule" "deny_node_to_pod_subnet" {
 }
 resource "azurerm_network_security_rule" "deny_pod_to_node_subnet" {
   name                        = "deny-pod-to-node-subnet"
-  resource_group_name         = var.resource_group_name         # module.aks_cluster_rg.rg_name
-  network_security_group_name = var.network_security_group_name # module.aks_nsg.network_security_group_name
+  resource_group_name         = var.resource_group_name
+  network_security_group_name = var.network_security_group_name
   priority                    = 102
   direction                   = "Inbound"
   access                      = "Deny"
