37d3fcdeec2d53352fa430e5581468d64e9958bb
Brian Flynn <bflynn@battech.com>
2023-06-27 15:52:57 -0600

    further modularizing nsg associations.

--- terraform/10_variables.tf
+++ terraform/10_variables.tf
@@ -62,20 +62,28 @@ variable "aks_config" {
       aks_default_pool = {
         node_address_prefixes = ["10.0.123.224/27"]
         pod_address_prefixes  = ["10.0.128.0/22"]
-        max_pods              = 32        # Needs to be determined by network math. 2^(node_mask-pod_mask)
-        zones                 = [1, 2, 3] # A /29 provides 1 per zone
-        name                  = "default"
-        Environment           = "defaultTag"
-        vm_size               = "Standard_B2s"
-        enable_auto_scaling   = true
-        os_disk_size_gb       = 30
-        min_count             = 1
-        max_count             = 3
+        max_pods              = 32 # Needs to be determined by network math. 
+        # 32= 2^(node_mask-pod_mask). 30 is a common config value.
+        # Other obvious configs would be 64:1, 128:1, 256:1
+        # which would map to common configs of 110/pool or 250/pool (max value)
+        # ex: /27 & /22 are /5 apart.  2^(27-22) = 2^5 That makes for 32:1 pods:nodes.
+        #     /29 is the smallest Azure subnet. Provides 3 usable IPs.
+        zones               = [1, 2, 3] # A /29 provides 1 per zone
+        name                = "default"
+        Environment         = "defaultTag"
+        vm_size             = "Standard_B2s"
+        enable_auto_scaling = true
+        os_disk_size_gb     = 30
+        min_count           = 1
+        max_count           = 3
       }
       aks_user_pool_1 = {
         node_address_prefixes = ["10.0.124.0/27"]
         pod_address_prefixes  = ["10.0.132.0/22"]
         max_pods              = 32 # Do not exceed 2^(node_mask-pod_mask)
+        # 32= 2^(node_mask-pod_mask). 30 is a common config value.
+        # Other obvious configs would be 64:1, 128:1, 256:1
+        # which would map to common configs of 110/pool or 250/pool (max value)
         # ex: /27 & /22 are /5 apart.  2^(27-22) = 2^5 That makes for 32:1 pods:nodes.
         #     /29 is the smallest Azure subnet. Provides 3 usable IPs.
         zones               = [1, 2, 3] # A /29 provides 1 per zone
@@ -91,6 +99,9 @@ variable "aks_config" {
         node_address_prefixes = ["10.0.124.32/27"]
         pod_address_prefixes  = ["10.0.136.0/22"]
         max_pods              = 32 # Needs to be determined by network math. 2^(node_mask-pod_mask)
+        # 32= 2^(node_mask-pod_mask). 30 is a common config value.
+        # Other obvious configs would be 64:1, 128:1, 256:1
+        # which would map to common configs of 110/pool or 250/pool (max value)
         # ex: /27 & /22 are /5 apart.  2^(27-22) = 2^5 That makes for 32:1 pods:nodes.
         #     /29 is the smallest Azure subnet. Provides 3 usable IPs.
         zones               = [1, 2, 3] # A /29 provides 1 per zone
