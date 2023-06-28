ebe26329123387ab6028c1840e18dae385beeba2
Brian Flynn <bflynn@battech.com>
2023-06-28 11:23:57 -0600

    aks_nsrs improvements

--- modules/aks_nsrs/main.tf
+++ modules/aks_nsrs/main.tf
@@ -11,25 +11,6 @@ resource "azurerm_network_security_rule" "allow_pod_subnet_outbound" {
   source_address_prefixes      = var.node_subnet_prefixes
   destination_address_prefixes = ["0.0.0.0/0"]
 }
-
-# concat(
-#   for_each = {
-#     for k, v
-#     in  var.node_pool_map : k => v
-#     if  k != "aks_default_pool"
-#   },
-#   var.node_pool_map[each.key].pod_address_prefixes
-# )
-
-# var.node_pool_map["aks_user_pool_1"].pod_address_prefixes,
-# var.node_pool_map["aks_user_pool_2"].pod_address_prefixes
-
-# for_each = {
-#   for k, v
-#   in var.aks_config.node_pool_map : k => v
-#   if k != "aks_default_pool" # excdlude default pool; 
-# }
-
 resource "azurerm_network_security_rule" "allow_pod_to_pod" {
   name                         = "allow-pod-to-pod-inbound"
   resource_group_name          = var.resource_group_name
