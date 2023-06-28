3074fc9fe7be89b009b1d21eb8a80b48aac27593
Brian Flynn <bflynn@battech.com>
2023-06-28 12:29:53 -0600

    aks_nsrs improvements

--- modules/aks_combined/main.tf
+++ modules/aks_combined/main.tf
@@ -41,9 +41,8 @@ module "aks_nsrs" {
   source                      = "../aks_nsrs"
   resource_group_name         = module.aks_rg.rg_name
   network_security_group_name = module.aks_cluster_nsg.network_security_group_name
-  for_each                    = var.aks_config.node_pool_map
-  node_subnet_prefixes        = concat(var.aks_config.node_pool_map[each.key].node_address_prefixes)
-  pod_subnet_prefixes         = concat(var.aks_config.node_pool_map[each.key].pod_address_prefixes)
+  node_subnet_prefixes        = module.aks_subnets.aks_node_address_prefixes
+  pod_subnet_prefixes         = module.aks_subnets.aks_pod_address_prefixes
 }
 # AKS Cluster
 resource "azurerm_kubernetes_cluster" "aks_cluster" {
