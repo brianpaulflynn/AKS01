32c240861d6d25325a2f8a1881e89946e5c47051
Brian Flynn <bflynn@battech.com>
2023-06-28 11:21:59 -0600

    aks_nsrs improvements

--- modules/aks_nsrs/main.tf
+++ modules/aks_nsrs/main.tf
@@ -1,73 +1,71 @@
 resource "azurerm_network_security_rule" "allow_pod_subnet_outbound" {
-  name                        = "allow-pod-subnet-outbound"
-  resource_group_name         = var.resource_group_name
-  network_security_group_name = var.network_security_group_name
-  priority                    = 100
-  direction                   = "Outbound"
-  access                      = "Allow"
-  protocol                    = "*"
-  source_port_range           = "*"
-  destination_port_range      = "*"
-  source_address_prefixes = concat(
-    var.node_pool_map["aks_user_pool_1"].pod_address_prefixes,
-    var.node_pool_map["aks_user_pool_2"].pod_address_prefixes
-  )
+  name                         = "allow-pod-subnet-outbound"
+  resource_group_name          = var.resource_group_name
+  network_security_group_name  = var.network_security_group_name
+  priority                     = 100
+  direction                    = "Outbound"
+  access                       = "Allow"
+  protocol                     = "*"
+  source_port_range            = "*"
+  destination_port_range       = "*"
+  source_address_prefixes      = var.node_subnet_prefixes
   destination_address_prefixes = ["0.0.0.0/0"]
 }
+
+# concat(
+#   for_each = {
+#     for k, v
+#     in  var.node_pool_map : k => v
+#     if  k != "aks_default_pool"
+#   },
+#   var.node_pool_map[each.key].pod_address_prefixes
+# )
+
+# var.node_pool_map["aks_user_pool_1"].pod_address_prefixes,
+# var.node_pool_map["aks_user_pool_2"].pod_address_prefixes
+
+# for_each = {
+#   for k, v
+#   in var.aks_config.node_pool_map : k => v
+#   if k != "aks_default_pool" # excdlude default pool; 
+# }
+
 resource "azurerm_network_security_rule" "allow_pod_to_pod" {
-  name                        = "allow-pod-to-pod-inbound"
-  resource_group_name         = var.resource_group_name
-  network_security_group_name = var.network_security_group_name
-  priority                    = 100
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "*"
-  source_port_range           = "*"
-  destination_port_range      = "*"
-  source_address_prefixes = concat(
-    var.node_pool_map["aks_user_pool_1"].pod_address_prefixes,
-    var.node_pool_map["aks_user_pool_2"].pod_address_prefixes
-  )
-  destination_address_prefixes = concat(
-    var.node_pool_map["aks_user_pool_1"].pod_address_prefixes,
-    var.node_pool_map["aks_user_pool_2"].pod_address_prefixes
-  )
+  name                         = "allow-pod-to-pod-inbound"
+  resource_group_name          = var.resource_group_name
+  network_security_group_name  = var.network_security_group_name
+  priority                     = 100
+  direction                    = "Inbound"
+  access                       = "Allow"
+  protocol                     = "*"
+  source_port_range            = "*"
+  destination_port_range       = "*"
+  source_address_prefixes      = var.pod_subnet_prefixes
+  destination_address_prefixes = var.pod_subnet_prefixes
 }
 resource "azurerm_network_security_rule" "deny_node_to_pod_subnet" {
-  name                        = "deny-node-to-pod-subnet"
-  resource_group_name         = var.resource_group_name
-  network_security_group_name = var.network_security_group_name
-  priority                    = 101
-  direction                   = "Inbound"
-  access                      = "Deny"
-  protocol                    = "*"
-  source_port_range           = "*"
-  destination_port_range      = "*"
-  source_address_prefixes = concat(
-    var.node_pool_map["aks_user_pool_1"].node_address_prefixes,
-    var.node_pool_map["aks_user_pool_2"].node_address_prefixes
-  )
-  destination_address_prefixes = concat(
-    var.node_pool_map["aks_user_pool_1"].pod_address_prefixes,
-    var.node_pool_map["aks_user_pool_2"].pod_address_prefixes
-  )
+  name                         = "deny-node-to-pod-subnet"
+  resource_group_name          = var.resource_group_name
+  network_security_group_name  = var.network_security_group_name
+  priority                     = 101
+  direction                    = "Inbound"
+  access                       = "Deny"
+  protocol                     = "*"
+  source_port_range            = "*"
+  destination_port_range       = "*"
+  source_address_prefixes      = var.node_subnet_prefixes
+  destination_address_prefixes = var.pod_subnet_prefixes
 }
 resource "azurerm_network_security_rule" "deny_pod_to_node_subnet" {
-  name                        = "deny-pod-to-node-subnet"
-  resource_group_name         = var.resource_group_name
-  network_security_group_name = var.network_security_group_name
-  priority                    = 102
-  direction                   = "Inbound"
-  access                      = "Deny"
-  protocol                    = "*"
-  source_port_range           = "*"
-  destination_port_range      = "*"
-  source_address_prefixes = concat(
-    var.node_pool_map["aks_user_pool_1"].pod_address_prefixes,
-    var.node_pool_map["aks_user_pool_2"].pod_address_prefixes
-  )
-  destination_address_prefixes = concat(
-    var.node_pool_map["aks_user_pool_1"].node_address_prefixes,
-    var.node_pool_map["aks_user_pool_2"].node_address_prefixes
-  )
+  name                         = "deny-pod-to-node-subnet"
+  resource_group_name          = var.resource_group_name
+  network_security_group_name  = var.network_security_group_name
+  priority                     = 102
+  direction                    = "Inbound"
+  access                       = "Deny"
+  protocol                     = "*"
+  source_port_range            = "*"
+  destination_port_range       = "*"
+  source_address_prefixes      = var.node_subnet_prefixes
+  destination_address_prefixes = var.node_subnet_prefixes
 }
--- modules/aks_nsrs/variables.tf
+++ modules/aks_nsrs/variables.tf
@@ -1,17 +1,19 @@
 variable "resource_group_name" { type = string }
 variable "network_security_group_name" { type = string }
-variable "node_pool_map" {
-  type = map(object({
-    node_address_prefixes = list(string)
-    pod_address_prefixes  = list(string)
-    max_pods              = string
-    name                  = string
-    Environment           = string
-    vm_size               = string
-    os_disk_size_gb       = string
-    zones                 = list(number)
-    enable_auto_scaling   = string
-    min_count             = string
-    max_count             = string
-  }))
-}
+variable "node_subnet_prefixes" { type = list(string) }
+variable "pod_subnet_prefixes" { type = list(string) }
+# variable "node_pool_map" {
+#   type = map(object({
+#     node_address_prefixes = list(string)
+#     pod_address_prefixes  = list(string)
+#     max_pods              = string
+#     name                  = string
+#     Environment           = string
+#     vm_size               = string
+#     os_disk_size_gb       = string
+#     zones                 = list(number)
+#     enable_auto_scaling   = string
+#     min_count             = string
+#     max_count             = string
+#   }))
+# }
